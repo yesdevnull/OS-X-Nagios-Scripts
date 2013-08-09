cde751a97b6b03d8fab89a67436f0c29b141f08f
Dan Barrett <dan@theblahman.net>
2013-08-09 20:38:32 +1000

    Added some comments for details

--- check_daylite_postgres.sh
+++ check_daylite_postgres.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#	Check Daylite Postgres
+#	by Dan Barrett
+#	http://yesdevnull.net
+
+#	v1.0 - 9 Aug 2013
+#	Initial release.
+
+#	Checks to make sure the user _dayliteserver has spawned at least one Postgres process
+
+#	Example:
+#	./check_daylite_postgres.sh
+
 result=`ps aux -o tty | grep _dayliteserver`
 if echo $result | grep -q postgres; then
 	printf "OK - Daylite has spawned at least one Postgres instance.\n"
