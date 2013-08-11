28bebcd803589cf74573da7ceda6633e9dd55ce6
Dan Barrett <dan@theblahman.net>
2013-08-11 16:47:13 +1000

    Remove any logic that breaks the routine too early

--- check_smart.sh
+++ check_smart.sh
@@ -34,30 +34,25 @@ then
 	graphString="|"
 fi
 
+# Did user want badSectors graph?
 if echo $graphs | grep -q "badSectors"
 then
 	badSectors=`/opt/local/libexec/nagios/smartctl -a $disk | grep -C 0 'Reallocated_Sector_Ct' | grep -E -o "[0-9]+" | tail -1 `
 	graphString="$graphString badSectors=$badSectors;"
 fi
 
+# Did user want reallocSectors graph?
 if echo $graphs | grep -q "reallocSectors"
 then
 	reallocSectors=`/opt/local/libexec/nagios/smartctl -a $disk | grep -C 0 'Reallocated_Event_Count' | grep -E -o "[0-9]+" | tail -1`
 	graphString="$graphString reallocSectors=$reallocSectors;"
 fi
 
+# Did user want powerOnHours graph?
 if echo $graphs | grep -q "powerOnHours"
 then
 	powerOnHours=`/opt/local/libexec/nagios/smartctl -a $disk | grep -C 0 'Power_On_Hours' | grep -E -o "[0-9]+" | tail -1`
 	graphString="$graphString powerOnHours=$powerOnHours;"
-fi
-
-#printf "bad sectors: $badSectors realloc: $reallocSectors\n\n"
-
-if [ $reallocSectors -gt 0 ] || [ $badSectors -gt 0 ]
-then
-	printf "CRITICAL - Drive has bad sectors, but S.M.A.R.T. status is OK $graphString\n"
-	exit 2
 fi
 
 if echo $resultString | grep -q "PASSED"
@@ -65,14 +60,16 @@ then
   	printf "OK - All S.M.A.R.T. attributes passed $graphString\n"
   	exit 0
 else
+	# Get the first line that is FAILING_NOW
 	failString=`echo $resultString | grep -C 0 'FAILING_NOW'`
+	# Now we make a human readable error message
 	if echo $failString | grep -q "Reallocated_Sector_Ct\|Reallocated_Event_Count"
 	then
 		printf "CRITICAL - Drive has bad sectors! $graphString\n"
 		exit 2
 	elif echo $failString | grep -q "Command_Timeout" 
 	then
-		printf "CRITICAL - Drive is having constant timeout issues! $graphString\n"
+		printf "CRITICAL - Drive is having constant timeout issues! Check any power sources. $graphString\n"
 		exit 2
 	else
 		printf "WARNING - Drive has failing S.M.A.R.T. attributes! $graphString\n"
